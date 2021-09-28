4d2253db553295aba5b63feccf6d691431bbd849
DarkuzDev <andrebrayan74@gmail.com>
2021-09-27 19:20:03 -0500

    [feat(games)]: Added Games List Functionality

--- components/ListGames/GamesList.js
+++ components/ListGames/GamesList.js
@@ -1,16 +1,41 @@
 import React from "react";
-
 import { map } from "lodash";
+import { Image, Grid } from "semantic-ui-react";
+import Link from "next/link";
 
 export default function GamesList({ games }) {
   return (
     <div className="games-list">
-      {map(games, (game) => (
-        <li>
-          <h3>{game.title}</h3>
-          <h3>{game.title}</h3>
-        </li>
-      ))}
+      <Grid>
+        <Grid.Row columns={5}>
+          {map(games, (game) => (
+            <Game game={game} />
+          ))}
+        </Grid.Row>
+      </Grid>
     </div>
+  );
+}
+
+function Game({ game }) {
+  return (
+    <Grid.Column className="games-list__game">
+      <Link href={`/${game.url}`}>
+        <a>
+          <div className="games-list__game-poster">
+            <Image src={game.poster.url} alt={game.title} />
+            <div className="games-list__game-poster-info">
+              {game.discount ? (
+                <span className="discount">-{game.discount}</span>
+              ) : (
+                <span></span>
+              )}
+              <span className="price">${game.price}</span>
+            </div>
+          </div>
+          <h2>{game.title}</h2>
+        </a>
+      </Link>
+    </Grid.Column>
   );
 }
