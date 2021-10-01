96f5729676a58f81f6ec6ab69ce15a737a36638f
DarkuzDev <andrebrayan74@gmail.com>
2021-10-01 07:48:15 -0500

    [feat(game)]: Added More Content in InfoContent

--- components/Game/InfoGame/InfoGame.js
+++ components/Game/InfoGame/InfoGame.js
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactPlayer from "react-player/lazy";
+import { format } from "date-fns";
+import { es } from "date-fns/locale";
+
 import CarouselScreenshots from "../CarouselScreenshots";
 
 export default function InfoGame({ game }) {
@@ -12,6 +15,13 @@ export default function InfoGame({ game }) {
       />
 
       <CarouselScreenshots title={game.title} screenshots={game.screenshots} />
+      <div className="info-game__content">
+        <div dangerouslySetInnerHTML={{ __html: game.summary }} />
+        <div className="info-game__content-date">
+          <h4>Fecha de lanzamiento: </h4>
+          <p>{format(new Date(game.releaseDate), "PPP", { locale: es })}</p>
+        </div>
+      </div>
     </div>
   );
 }
